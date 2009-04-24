85573e41c86957aac6c971655f01c2389282d96c
Scott Shawcroft <scott.shawcroft@gmail.com>
2009-04-23 21:08:37 -0700

    DistroHistory handles branches too.

--- crawl/utils/history.py
+++ crawl/utils/history.py
@@ -250,21 +250,25 @@ class DistroHistory:
 
 if __name__=="__main__":
   if len(sys.argv)<2:
-    print sys.argv[0],"<package>","[threshold]","[distro]"
+    print sys.argv[0],"<package>","[threshold]","[distro]","[branch]"
     sys.exit(1)
   #VERBOSE = True
   p = sys.argv[1]
   t = 255
   d = None
+  b = "current"
   if len(sys.argv)>2:
     t = sys.argv[2]
   
   if len(sys.argv)>3:
     d = sys.argv[3]
+  
+  if len(sys.argv)>4:
+    b = sys.argv[4]
 
   p = PackageHistory(p,t)
   print p
   
   if d != None:
-    d = DistroHistory(d,[p],"current")
+    d = DistroHistory(d,[p],b)
     print d.timeline
