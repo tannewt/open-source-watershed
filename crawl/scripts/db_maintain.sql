da6ef8e33ef45b32b91384347ec53b64d139ed90
tannewt <tannewt@wintergreen.(none)>
2009-07-02 02:02:43 -0700

    db maintenance script

---
+++ crawl/scripts/db_maintain.sql
@@ -0,0 +1,5 @@
+VACUUM ANALYZE;
+-- CLUSTER dreleases USING dreleases_pkg_idx;
+CLUSTER dreleases_pkg_idx ON dreleases;
+-- CLUSTER ureleases USING ureleases_pkg_idx;
+CLUSTER ureleases_pkg_idx ON ureleases;
