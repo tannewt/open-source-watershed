882674c643a74405ab897118d817fafdd830775c
tannewt <tannewt@wintergreen.(none)>
2009-07-13 18:10:30 -0700

    Badge api server code.

---
+++ site/api/views.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from django.http import HttpResponse
+from utils.history import *
+
+def package(request):
+	p = request.GET["package"]
+	ph = PackageHistory(p)
+	distros = get_all_distros([ph], "current")
+	cb = request.GET["cb"]
+	response = cb+'({"package":"%s","latest":"%s","distros":['%(ph.name, ph.timeline[-1])
+	distro_entries = []
+	for distro in distros:
+		timeline = distro.get_obsoletion_timeline()
+		v = distro.get_pkg(ph.name)[-1]
+		if v==0:
+			v = "--"
+		if len(timeline)>0 and timeline[-1]==0.0:
+			utd = "true"
+		else:
+			utd = "false"
+		distro_entries.append('{"logo": "%s.png", "version": "%s", "uptodate": %s}'%(distro.name, v, utd))
+	response += ",".join(distro_entries)+ "]})"
+	return HttpResponse(response)
