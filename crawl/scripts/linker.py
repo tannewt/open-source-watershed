9914c34727d4c06804b9f8b0b56b4ea7e860e6ba
tannewt <tannewt@wintergreen.(none)>
2009-07-14 20:12:09 -0700

    move linker to rev3

--- crawl/scripts/linker.py
+++ crawl/scripts/linker.py
@@ -1,6 +1,7 @@
+# -*- coding: utf-8 -*-
 import sys
 import os
-import MySQLdb as mysql
+import psycopg2 as db
 
 sys.path.append(os.getcwd())
 from utils import helper
@@ -8,26 +9,26 @@ from utils import helper
 HOST, USER, PASSWORD, DB = helper.mysql_settings()
 
 if len(sys.argv)<2:
-  print sys.argv[0],"<cmd>"
+	print sys.argv[0],"<cmd>"
 
 cmd = sys.argv[1]
-con = mysql.connect(host=HOST,user=USER,passwd=PASSWORD,db=DB)
+con = db.connect(host=HOST,user=USER,passwd=PASSWORD,db=DB)
 cur = con.cursor()
 
 BY_HAND_LINK_ID = 5
 
 if cmd == "list":
-  cur.execute("SELECT p1.name, p2.name FROM packages AS p1, packages AS p2, links WHERE p1.id = links.package_id1 AND p2.id = links.package_id2 AND links.link_source_id = %s ORDER BY p1.name, p2.name",(BY_HAND_LINK_ID,))
-  for p1, p2 in cur:
-    print p1,"<--",p2
+	cur.execute("SELECT p1.name, p2.name FROM packages AS p1, packages AS p2, links WHERE p1.id = links.package_tgt AND p2.id = links.package_src ORDER BY p1.name, p2.name")
+	for p1, p2 in cur:
+		print p1,"<--",p2
 elif cmd == "link":
-  cur.execute("INSERT INTO links (package_id1, package_id2, link_source_id) VALUES ((SELECT id FROM packages WHERE name=%s),(SELECT id FROM packages WHERE name=%s), %s)",(sys.argv[2],sys.argv[3],BY_HAND_LINK_ID))
+	cur.execute("INSERT INTO links (package_tgt, package_src) VALUES ((SELECT id FROM packages WHERE name=%s),(SELECT id FROM packages WHERE name=%s))",(sys.argv[2],sys.argv[3]))
 elif cmd == "unlink":
-  pass
+	pass
 else:
-  print "list -- lists all handmade links"
-  print "link <p1> <p2> -- link p1 and p2 by hand"
-  print "unlink <p1> <p2> -- unlink p1 and p2"
+	print "list -- lists all handmade links"
+	print "link <p1> <p2> -- link p1 and p2 by hand"
+	print "unlink <p1> <p2> -- unlink p1 and p2"
 
 con.commit()
 con.close()
