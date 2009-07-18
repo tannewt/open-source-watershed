f8524e0c9f8bc71ae11886247659ecf8b52a4bf2
tannewt <tannewt@wintergreen.(none)>
2009-07-18 12:47:23 -0700

    change api url

--- site/js/package_badge.js
+++ site/js/package_badge.js
@@ -1,5 +1,5 @@
 package_badge = function(package, element) {
-  $.getJSON("http://127.0.0.1:8000/api/0.1/package.json?cb=?", {package: package}, function(data) {
+  $.getJSON("http://api.oswatershed.org/api/0.1/package.json?cb=?", {package: package}, function(data) {
 			$(element).css({"font-family": "sans-serif"})
       $(element).append('<a href="http://oswatershed.org/"><img style="border: none; padding-right: 4px;" src="http://static.oswatershed.org/small_logo.png" /></a>');
       $(element).append('<a style="color: black; text-decoration: none;" id="pkglink" href="http://oswatershed.org/pkg/'+data.package+'"></a>');
